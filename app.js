2f968024ae88d9ce96715122896d824a305823d0
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-10-06 17:19:00 +0200

    simple attack

--- app.js
+++ app.js
@@ -13,5 +13,26 @@ new Vue({
             this.monsterHealth = 100;
             this.turns = [];
         },
+        attack: function () {
+            var damage = this.calculateDamage(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: 'Player hits Monster for ' + damage
+            });
+
+            this.monsterAttacks();
+        },
+        monsterAttacks: function() {
+            var damage = this.calculateDamage(5, 12);
+            this.playerHealth -= damage;
+            this.turns.unshift({
+                isPlayer: false,
+                text: 'Monster hits Player for ' + damage
+            });
+        },
+        calculateDamage: function(min, max) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        }
     }
 });
